c0f4771254676c2b0dfc9a87d5f0ad8bb70773cd
keplerde <keplerde@gmail.com>
2022-12-24 17:33:27 +0100

    переписать старый метод на новый

--- 01/lesson_1.py
+++ 01/lesson_1.py
@@ -2,7 +2,42 @@
 # 2. Узнать версию гугл хрома
 # 3. Качаем архив с версией под нужную ОС (вебдрайвер)
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from auth_data import username, password
+import random
+
+
+def login(username, password):
+    try:
+        browser = webdriver.Chrome('E:\PycharmProjects\\Insta\\01\\chromedriver.exe')
+        browser.get('https://www.instagram.com')
 
 
 
+        time.sleep(random.randrange(3, 5))
 
+        username_input = browser.find_element_by_name('username')
+        username_input.clear()
+        username_input.send_keys(username)
+
+        time.sleep(2)
+
+        password_input = browser.find_element_by_name('password')
+        password_input.clear()
+        password_input.send_keys(password)
+
+        password_input.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+        browser.close()
+        browser.quit()
+
+    except Exception as ex:
+        print(ex)
+        browser.close()
+        browser.quit()
+
+
+login(username, password)
